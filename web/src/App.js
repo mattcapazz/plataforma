c7d7ad536d6e0a7281154663bdaa6b7a0665d034
Matt <mattcapazz@gmail.com>
2021-11-02 16:19:26 +0000

    Navbar

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import Navbar from "./components/navbar"
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    
     <div className="App">
-      <Navbar></Navbar>
+      <Navbar />
     </div>
   );
 }
