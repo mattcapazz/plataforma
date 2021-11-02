e24a3b12054979f9f925c2dcf91bfcfae1d9b242
Mario Fernandes <m4r10i@gmail.com>
2021-11-02 20:21:12 +0000

    Carousel width ajeitado e navbar posicionada

--- web/src/components/carousel.jsx
+++ web/src/components/carousel.jsx
@@ -1,27 +1,23 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import image1 from "./img/1.jpg";
+import image2 from "./img/2.jpeg";
+import image3 from "./img/3.jpg";
+import "./carousel.css";
 
 export default class carousel extends React.Component {
-    render() {
+  render() {
     return (
       <Carousel>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
+          <img className="d-block w-100" src={image1} alt="First slide" />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="../img/2.jpeg"
-            alt="Second slide"
-          />
+          <img className="d-block w-100" src={image2} alt="Second slide" />
 
           <Carousel.Caption>
             <h3>Second slide label</h3>
@@ -29,11 +25,7 @@ export default class carousel extends React.Component {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src={image3} alt="Third slide" />
 
           <Carousel.Caption>
             <h3>Third slide label</h3>
@@ -44,5 +36,5 @@ export default class carousel extends React.Component {
         </Carousel.Item>
       </Carousel>
     );
-}
+  }
 }
--- web/src/components/navbar.jsx
+++ web/src/components/navbar.jsx
@@ -7,7 +7,7 @@ export default class Navbar extends React.Component {
       <ReactBootStrap.Navbar bg="light" expand="lg">
         <ReactBootStrap.Container fluid>
           <ReactBootStrap.Navbar.Brand href="#">
-            Navbar scroll
+            LOGO
           </ReactBootStrap.Navbar.Brand>
           <ReactBootStrap.Navbar.Toggle aria-controls="navbarScroll" />
           <ReactBootStrap.Navbar.Collapse id="navbarScroll">
@@ -16,27 +16,31 @@ export default class Navbar extends React.Component {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <ReactBootStrap.Form className="d-flex">
-                <ReactBootStrap.FormControl
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <ReactBootStrap.Button variant="outline-success">
-                  Search
-                </ReactBootStrap.Button>
-              </ReactBootStrap.Form>
+              
+                <ReactBootStrap.Form className="d-flex">
+                  <ReactBootStrap.FormControl
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+
+                  <ReactBootStrap.Button variant="outline-success">
+                    Search
+                  </ReactBootStrap.Button>
+                </ReactBootStrap.Form>
+              
             </ReactBootStrap.Nav>
-            <nav className="col-md-5 mx-auto ">
+
+            <nav className="col-md-4">
               <ReactBootStrap.Nav>
                 <ReactBootStrap.Nav.Link href="#action1">
-                  <h2>Home</h2>
+                  <h3>Registar</h3>
                 </ReactBootStrap.Nav.Link>
                 <ReactBootStrap.Nav.Link href="#action2">
-                  Link
+                  <h3>Iniciar Sessão</h3>
                 </ReactBootStrap.Nav.Link>
                 <ReactBootStrap.NavDropdown
-                  title="Link"
+                  title="Ajuda"
                   id="navbarScrollingDropdown"
                 >
                   <ReactBootStrap.NavDropdown.Item href="#action3">
@@ -50,9 +54,6 @@ export default class Navbar extends React.Component {
                     Something else here
                   </ReactBootStrap.NavDropdown.Item>
                 </ReactBootStrap.NavDropdown>
-                <ReactBootStrap.Nav.Link href="#" disabled>
-                  Link
-                </ReactBootStrap.Nav.Link>
               </ReactBootStrap.Nav>
             </nav>
           </ReactBootStrap.Navbar.Collapse>
