cf93c29697bb80f13bd87dd38252463adc508cc2
Mario Fernandes <m4r10i@gmail.com>
2021-11-02 19:39:39 +0000

    inicio construçao carousel

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Carousel from "./components/carousel";
 import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Carousel />
     </div>
   );
 }
--- web/src/components/navbar.jsx
+++ web/src/components/navbar.jsx
@@ -4,31 +4,33 @@ import * as ReactBootStrap from "react-bootstrap";
 export default class Navbar extends React.Component {
   render() {
     return (
-        <ReactBootStrap.Navbar bg="light" expand="lg">
-          <ReactBootStrap.Container fluid>
-            <ReactBootStrap.Navbar.Brand href="#">
-              Navbar scroll
-            </ReactBootStrap.Navbar.Brand>
-            <ReactBootStrap.Navbar.Toggle aria-controls="navbarScroll" />
-            <ReactBootStrap.Navbar.Collapse id="navbarScroll">
-              <ReactBootStrap.Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: "100px" }}
-                navbarScroll
-              >
-                <ReactBootStrap.Form className="d-flex">
-                  <ReactBootStrap.FormControl
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <ReactBootStrap.Button variant="outline-success">
-                    Search
-                  </ReactBootStrap.Button>
-                </ReactBootStrap.Form>
+      <ReactBootStrap.Navbar bg="light" expand="lg">
+        <ReactBootStrap.Container fluid>
+          <ReactBootStrap.Navbar.Brand href="#">
+            Navbar scroll
+          </ReactBootStrap.Navbar.Brand>
+          <ReactBootStrap.Navbar.Toggle aria-controls="navbarScroll" />
+          <ReactBootStrap.Navbar.Collapse id="navbarScroll">
+            <ReactBootStrap.Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <ReactBootStrap.Form className="d-flex">
+                <ReactBootStrap.FormControl
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <ReactBootStrap.Button variant="outline-success">
+                  Search
+                </ReactBootStrap.Button>
+              </ReactBootStrap.Form>
+            </ReactBootStrap.Nav>
+            <nav className="col-md-5 mx-auto ">
+              <ReactBootStrap.Nav>
                 <ReactBootStrap.Nav.Link href="#action1">
-                  Home
+                  <h2>Home</h2>
                 </ReactBootStrap.Nav.Link>
                 <ReactBootStrap.Nav.Link href="#action2">
                   Link
@@ -52,9 +54,10 @@ export default class Navbar extends React.Component {
                   Link
                 </ReactBootStrap.Nav.Link>
               </ReactBootStrap.Nav>
-            </ReactBootStrap.Navbar.Collapse>
-          </ReactBootStrap.Container>
-        </ReactBootStrap.Navbar>
+            </nav>
+          </ReactBootStrap.Navbar.Collapse>
+        </ReactBootStrap.Container>
+      </ReactBootStrap.Navbar>
     );
   }
 }
