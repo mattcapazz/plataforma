b829aa81da7048f9446d5ecbb48af6ec5a1e801d
Mario Fernandes <m4r10i@gmail.com>
2021-11-02 16:03:53 +0000

    primeiro teste

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/navbar"
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hello World!</code>
-        </p>
-      </header>
+      <Navbar></Navbar>
     </div>
   );
 }
